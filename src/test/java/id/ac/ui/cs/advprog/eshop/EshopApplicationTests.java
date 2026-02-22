c3bdc3a5f3605694c89213b348f31def254242d7
sahilaathia <athiasahila@gmail.com>
2026-02-22 14:37:01 +0700

    add assertion to EshopApplicationTests

--- src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
@@ -2,12 +2,14 @@ package id.ac.ui.cs.advprog.eshop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 
 @SpringBootTest
 class EshopApplicationTests {
 
     @Test
     void contextLoads() {
-        EshopApplication.main(new String[] {});
+        assertDoesNotThrow(() -> EshopApplication.main(new String[] {}), "Application should start without throwing exceptions");
     }
 }
